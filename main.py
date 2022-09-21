0ee1a44447d669c60e0a7e89ee4ef64ce8368644
Frederik Snedevind <fsnedevind@hotmail.com>
2022-09-21 18:51:29 +0000

    Added mouseDrag to the mix

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ def pressKeyRandomTime(key):
     pyautogui.keyUp(key)
 
 
+def mouseDrag():
+    pyautogui.dragTo(random.randint(0, 1080), random.randint(0, 900), button='left', duration=1)
+
 
 
 ### Main
-
 for a in range(4):
     print("Beginning in: " + str(4-a))
     sleep(1)
@@ -28,10 +30,13 @@ while(True):
     list = []
 
     for a in range(random.choice(range(1, 4))):
-        list.append(random.choice(["up", "down"]))
+        list.append(random.choice(["up", "down", "drag"]))
 
-    for a in list:
-        pressKeyRandomTime(a)
+    for key in list:
+        if key == "up" or key == "down":
+            pressKeyRandomTime(key)
+        else:
+            mouseDrag()
 
     now = datetime.now()
     currentTime = now.strftime("%H:%M:%S")
